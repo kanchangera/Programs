e098a91c42ee0370d554378401b43e640cf71ac6
Kanchan Gera <kanchangera2010@gmail.com>
2021-03-25 21:32:40 +0530

    Printout only elements of the list that are less than 5

---
+++ LessThan5.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 20 07:55:26 2021
+
+@author: kanch
+"""
+
+#A program that prints out all the elements of the list that are less than 5.
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+print("Printout all the elements of the list that are less than 5")
+for i in a:
+
+    if i<5:
+        
+     print(i)
+     
+     #i=i+1
